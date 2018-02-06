10bf158cb5c23983de38f1109a49feb88e3aca41
Jay reddy <jayreddy199@gmail.com>
2018-02-06 15:00:25 +0530

    Fetching data from DB using Models and display in Views

--- blog/blogsite/views.py
+++ blog/blogsite/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def posts(request):
-	return render(request, 'blogsite/posts.html', {})
+	posts = Post.objects.all()
+
+	#return render(request, 'blogsite/posts.html', {})
+	return render(request, 'blogsite/posts.html', {'posts': posts})
