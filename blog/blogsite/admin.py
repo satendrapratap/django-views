f0e35ca6367e642d9ab83835d0551136b935a83a
Jay reddy <jayreddy199@gmail.com>
2018-02-06 14:02:18 +0530

    Django project example with app with empty views

---
+++ blog/blogsite/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from blogsite.models import Post
+from blogsite.models import Comment
+
+# Register your models here.
+admin.site.register(Post)
+admin.site.register(Comment)
